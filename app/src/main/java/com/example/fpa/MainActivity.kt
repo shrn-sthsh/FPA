bf6a5e662a65db950cabc0e1ec5fce8c99eb43bb
Sharan Sathish <shrnsthsh@gmail.com>
2022-09-11 01:00:17 -0400

    Establish Firebase Connection; Make Google Login

--- app/src/main/java/com/example/fpa/MainActivity.kt
+++ app/src/main/java/com/example/fpa/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.example.fpa
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 
-class MainActivity : AppCompatActivity() {
+class MainActivity: AppCompatActivity() {
+
+    private companion object MainActivity {
+        private const val TAG = "MainActivity"
+    }
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var client: GoogleSignInClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        auth = FirebaseAuth.getInstance()
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        client = GoogleSignIn.getClient(this, gso)
+
+        var logoutButton = findViewById<Button>(R.id.logoutButton)
+        logoutButton.setOnClickListener {
+            Log.i(TAG, "Logout")
+            auth.signOut()
+            client.signOut()
+            val logoutIntent = Intent(this, LoginActivity::class.java)
+            logoutIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(logoutIntent)
+        }
+
     }
+
 }
